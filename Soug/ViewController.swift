7f5ac6df03ea65999335b2c7f0c263801050668b
mentor33 <mobark.k11@outlook.com>
2019-12-17 18:35:43 +0200

    Initial Commit

---
+++ Soug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soug
+//
+//  Created by Mobark on 17/12/2019.
+//  Copyright © 2019 Mobark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
